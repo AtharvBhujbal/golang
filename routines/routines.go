31925dd998ccd9096e0db36cae067b75e163f5af
Atharv Bhujbal <126101769+AtharvBhujbal@users.noreply.github.com>
2024-09-25 00:20:28 +0530

    Mutex Lock and Unlock

--- routines/routines.go
+++ routines/routines.go
@@ -9,10 +9,12 @@ import (
 
 // Writing a keyword "go" before a function tells a compiler to async call the function
 // Without the sync package and wait counter the program will end and it will not return any values
+// We should use the Lock unlock memory locations to stay out of Deadlock situations
 
+var m = sync.Mutex{}
 var wg = sync.WaitGroup{}
 var dbData = []string{"id1", "id2", "id3", "id4", "id5"}
-var result = []string{}
+var results = []string{}
 
 func main() {
 	t0 := time.Now()
@@ -22,12 +24,15 @@ func main() {
 	}
 	wg.Wait()
 	fmt.Printf("\nTotal execution time: %v", time.Since(t0))
-	fmt.Printf("\nThe results are %v", result)
+	fmt.Printf("\nThe results are %v", results)
 }
 
 func dbCall(i int) {
 	var delay float32 = rand.Float32() * 2000
 	time.Sleep(time.Duration(delay) * time.Millisecond)
 	fmt.Printf("\nThe result from database is: %v", dbData[i])
+	m.Lock()
+	results = append(results, dbData[i])
+	m.Unlock()
 	wg.Done()
 }
