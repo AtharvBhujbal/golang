d1a51b41fec4b0092d4e7526718d39e97656360d
Atharv Bhujbal <126101769+AtharvBhujbal@users.noreply.github.com>
2024-09-25 20:14:03 +0530

    Channel

---
+++ channels/channel.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+// Channels
+// 1. Channels hold data, anything from range, int or slices
+// 2. Thread safe i.e. avoid data erases while reading and writing from databases
+// 3. Listen for data i.e. block code execution until read or write data is done
+// IMP Point
+//	- When a val is set to a channel it blocks the code execution until the data is read from it
+// ex:
+// func example(){
+//	var c = make(chan int)
+//	c<-1 // a val 1 is set to channel, the code execution is blocked until the val is read from channel leading to "DEADLOCK"
+//	var i = <- c // The code never reaches here
+//	fmt.Println(i)
+// }
+
+func main() {
+	var c = make(chan int)
+	go process(c)
+	for i := range c {
+		fmt.Println(i)
+	}
+}
+
+func process(c chan int) {
+	defer close(c)
+	for i := 0; i < 5; i++ {
+		c <- i
+	}
+}
