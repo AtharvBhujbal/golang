6d904c1239395b404e3f2231614fffc1c2c231af
Atharv Bhujbal <126101769+AtharvBhujbal@users.noreply.github.com>
2024-09-26 20:15:20 +0530

    Generic with 'any' data type

--- generics/gen.go
+++ generics/gen.go
@@ -4,6 +4,9 @@ import "fmt"
 
 // Generics are "method overloading" in go
 // You can specify the type of input data type in a square brackets
+// New data type "any" can also be used
+// It can not be used for the sumSlice function as everything is not compatible with addition operator
+// Ex : we cannot use addition operator with string (strings are immutable in go)
 
 func main() {
 	var intSlice = []int32{5, 6, 7}
@@ -12,6 +15,7 @@ func main() {
 	fmt.Println(sumSlice(floatSlice1))
 	var floatSlice2 = []float64{9.1611, 7.2723, 9.133773}
 	fmt.Println(sumSlice(floatSlice2))
+	fmt.Println(isEmpty(floatSlice1))
 
 }
 func sumSlice[T int32 | float32 | float64](slice []T) T {
@@ -20,4 +24,8 @@ func sumSlice[T int32 | float32 | float64](slice []T) T {
 		sum += v
 	}
 	return sum
+}
+
+func isEmpty[T any](slice []T) bool {
+	return len(slice) == 0
 }
